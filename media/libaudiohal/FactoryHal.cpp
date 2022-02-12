583fe660310719c059997f9339deb74c82228b74
TARKZiM <tom8476oo@gmail.com>
2022-02-12 18:41:32 +0800

    libaudiohal: Bring back 2.0 HAL
    [TARKZiM] Adapt to 14.
    
    Change-Id: I1800712582df740db9f60d29e22cda18c4895a96

--- media/libaudiohal/FactoryHal.cpp
+++ media/libaudiohal/FactoryHal.cpp
@@ -50,7 +50,7 @@ using InterfaceName = std::pair<std::string, std::string>;
  * This list need to keep sync with AudioHalVersionInfo.VERSIONS in
  * media/java/android/media/AudioHalVersionInfo.java.
  */
-static const std::array<AudioHalVersionInfo, 5> sAudioHALVersions = {
+static const std::array<AudioHalVersionInfo, 6> sAudioHALVersions = {
     // TODO: remove this comment to get AIDL
     // AudioHalVersionInfo(AudioHalVersionInfo::Type::AIDL, 1, 0),
     AudioHalVersionInfo(AudioHalVersionInfo::Type::HIDL, 7, 1),
@@ -58,6 +58,7 @@ static const std::array<AudioHalVersionInfo, 5> sAudioHALVersions = {
     AudioHalVersionInfo(AudioHalVersionInfo::Type::HIDL, 6, 0),
     AudioHalVersionInfo(AudioHalVersionInfo::Type::HIDL, 5, 0),
     AudioHalVersionInfo(AudioHalVersionInfo::Type::HIDL, 4, 0),
+    AudioHalVersionInfo(AudioHalVersionInfo::Type::HIDL, 2, 0),
 };
 
 static const std::map<AudioHalVersionInfo::Type, InterfaceName> sDevicesHALInterfaces = {
